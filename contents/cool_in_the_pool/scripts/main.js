bf3b3ef5e2ac70c31aa42380c02ef381e72a3e35
Daniel Cavalcante <danielcavalcante@protonmail.com>
2022-09-02 15:28:24 -0300

    substituted images that are not public domain

--- contents/cool_in_the_pool/scripts/main.js
+++ contents/cool_in_the_pool/scripts/main.js
@@ -1,27 +1,27 @@
-function imgCycler (mySrc) {
-  if (mySrc === 'images/template.jpeg') {
-    myImage.setAttribute('src','images/template-color-inverted.jpeg');
-    myImage.setAttribute('title','a puppie NOT cool in the pool')
-  };
+// function imgCycler (mySrc) {
+//   if (mySrc === 'images/template.jpeg') {
+//     myImage.setAttribute('src','images/template-color-inverted.jpeg');
+//     myImage.setAttribute('title','a puppie NOT cool in the pool')
+//   };
   
-  if (mySrc === 'images/template-color-inverted.jpeg') {
-    myImage.setAttribute('src','images/template-upside-down.jpeg');
-    myImage.setAttribute('title','a cool puppie?')
-  };
+//   if (mySrc === 'images/template-color-inverted.jpeg') {
+//     myImage.setAttribute('src','images/template-upside-down.jpeg');
+//     myImage.setAttribute('title','a cool puppie?')
+//   };
 
-  if (mySrc === 'images/template-upside-down.jpeg') {
-    myImage.setAttribute('src','images/template.jpeg');
-    myImage.setAttribute('title','puppie cool in the pool')
-  };
-}
+//   if (mySrc === 'images/template-upside-down.jpeg') {
+//     myImage.setAttribute('src','images/template.jpeg');
+//     myImage.setAttribute('title','puppie cool in the pool')
+//   };
+// }
 
-const myImage = document.getElementById('banner');
+// const myImage = document.getElementById('banner');
 
-// call arrow function on click
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute('src');
-  imgCycler(mySrc);
-}
+// // call arrow function on click
+// myImage.onclick = () => {
+//   const mySrc = myImage.getAttribute('src');
+//   imgCycler(mySrc);
+// }
 
 // let myButton = document.querySelector('button');
 // let myHeading = document.querySelector('h1');
